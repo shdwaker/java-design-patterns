01d6c39e18b902a43a94b686b4ca66708b8be845
yihonglei <yihonglei@baofeng.com>
2018-07-17 16:47:27 +0800

    单例模式8种写法

---
+++ DesignPattern/design-pattern/src/main/java/com/lanhuigu/design/singleton/Test.java
@@ -0,0 +1,14 @@
+package com.lanhuigu.design.singleton;
+
+
+/**
+ * @author yihonglei
+ * @version 1.0.0
+ * @ClassName: Test
+ * @Package: com.lanhuigu.design.singleton
+ * @date 2018/7/17 10:28
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}

0ebe7a15c668577ce0ff73ceffa3ee21145bce4a
yihonglei <yihonglei@bcpeople.com>
2019-02-26 17:59:51 +0800

    枚举创建单例

---
+++ DesignPattern/design-pattern/src/main/java/com/lanhuigu/design/singleton/demo8/Singleton02.java
@@ -0,0 +1,13 @@
+package com.lanhuigu.design.singleton.demo8;
+
+/**
+ * @author yihonglei
+ * @date 2019/2/26 17:58
+ */
+public enum  Singleton02 {
+    INSTANCE;
+
+    public void anyMethod() {
+
+    }
+}

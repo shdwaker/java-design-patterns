3af0621bc4522bf9e83f33f0f4e398289348c29a
yihonglei <yihonglei@hqzhuanche.com>
2020-02-13 17:23:57 +0800

    formate code

--- src/main/java/com/jpeony/design/patterns/template/demo1/ConcreteTemplate.java
+++ src/main/java/com/jpeony/design/patterns/template/demo1/ConcreteTemplate.java
@@ -1,16 +1,9 @@
 package com.jpeony.design.patterns.template.demo1;
 
 /**
- * @author yihonglei
- * @version 1.0.0
- * @ClassName: ConcreteTemplate
- * @Package: com.lanhuigu.design.template.demo1
- * @date 2018/3/20 11:34
- */
-/**
+ * ConcreteClass: 继承抽象类，完成算法中与特定子类相关的步骤。
  *
  * @author yihonglei
- * @date 2018/8/21 10:20
  */
 public class ConcreteTemplate extends Template {
     @Override
--- src/main/java/com/jpeony/design/patterns/template/demo1/Test.java
+++ src/main/java/com/jpeony/design/patterns/template/demo1/Test.java
@@ -2,15 +2,6 @@ package com.jpeony.design.patterns.template.demo1;
 
 /**
  * @author yihonglei
- * @version 1.0.0
- * @ClassName: Test
- * @Package: com.lanhuigu.design.template.demo1
- * @date 2018/3/20 11:35
- */
-/**
- *
- * @author yihonglei
- * @date 2018/8/21 10:20
  */
 public class Test {
     public static void main(String[] args) {

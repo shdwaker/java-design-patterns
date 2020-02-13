2c2291c09e2f79adf78323cc60e0637e777cbff3
yihonglei <yihonglei@hqzhuanche.com>
2020-02-13 12:38:19 +0800

    format code

--- src/main/java/com/jpeony/design/patterns/factory/FactoryMethodTest.java
+++ src/main/java/com/jpeony/design/patterns/factory/FactoryMethodTest.java
@@ -1,11 +1,11 @@
 package com.jpeony.design.patterns.factory;
+
 /**
  * 工厂方法模式应用程序测试，通过工厂创建具体产品对象。
+ *
  * @author yihonglei
- * @date 2018/8/21 10:20
  */
 public class FactoryMethodTest {
-    // main
     public static void main(String[] args) {
         StudentWorkFactory studentWorkFactory = new StudentWorkFactory();
         studentWorkFactory.getWork().doWork();
--- src/main/java/com/jpeony/design/patterns/factory/StudentWork.java
+++ src/main/java/com/jpeony/design/patterns/factory/StudentWork.java
@@ -2,10 +2,10 @@ package com.jpeony.design.patterns.factory;
 
 /**
  * 具体产品(Concrete Product)角色：实现接口对象为具体对象
+ *
  * @author yihonglei
- * @date 2018/8/21 10:20
  */
-public class StudentWork implements Work{
+public class StudentWork implements Work {
 
     @Override
     public void doWork() {
